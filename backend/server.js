dacf99c89d75f771dc5f0679ae3e73ba00d1abe9
MAYUR JADHAV <83859709+MJ41739@users.noreply.github.com>
2025-11-07 00:59:06 +0530

    Update CORS origin to use environment variables

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ connectCloudinary()
 app.use(express.json())
 app.use(cors({
   // origin: ['http://localhost:5173','http://localhost:5174'],
-  origin: ['https://hospitalmanagementsystem-nextastra.vercel.app','https://admin-hospitalmanagementsystem-nextastra.vercel.app'],
+  origin: [`${process.env.ADMIN_URL}`,`${process.env.USER_URL}`],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }))
